3e9c9c68b854e2aa9957261fbe392f4e40267117
You Ken Sang <youks95@bluegames.co.kr>
2018-03-13 12:11:40 +0900

    콘솔 에디터 프로 추가 cvs2data를 활용하는 함수 추가

---
+++ editor확장/DataManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// todo: 에셋자체가 너무 커질경우 동적으로 읽고, 필요없으면 내리는 작업을 추가한다.
+/// </summary>
+public class DataManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static DataManager s_Instance = null;
+
+    public static DataManager Instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                s_Instance = FindObjectOfType(typeof(DataManager)) as DataManager;
+            }
+
+            if (s_Instance == null)
+            {
+                GameObject obj = new GameObject("_DataManager");
+                s_Instance = obj.AddComponent(typeof(DataManager)) as DataManager;
+                DontDestroyOnLoad(obj);
+            }
+
+            return s_Instance;
+        }
+    }
+
+    #endregion Singleton
+
+    public List<ScriptableObject> _allDatas = new List<ScriptableObject>();
+    private static bool _isInit = false;
+
+    public void Init()
+    {
+        _allDatas.AddRange(Resources.LoadAll<ScriptableObject>("DataAssets"));
+    }
+
+    public T GetData<T>(int key)
+    {
+        if (!_isInit)
+        {
+            _isInit = true;
+            Init();
+        }
+        foreach (ScriptableObject s in _allDatas)
+        {
+            if (s.GetType().GetField("arrayData").FieldType.GetElementType() == typeof(T))
+            {
+                return (T)s.GetType().GetMethod("FindByKey").Invoke(s, new object[] { key });
+            }
+        }
+        Debug.LogError("not find data type " + typeof(T) + " key=" + key);
+        return default(T);
+    }
+    public T GetData<T>(string key)
+    {
+        if (!_isInit)
+        {
+            _isInit = true;
+            Init();
+        }
+        foreach (ScriptableObject s in _allDatas)
+        {
+            if (s.GetType().GetField("arrayData").FieldType.GetElementType() == typeof(T))
+            {
+                return (T)s.GetType().GetMethod("FindByKey").Invoke(s, new object[] { key });
+            }
+        }
+        Debug.LogError("not find data type " + typeof(T) + " key=" + key);
+        return default(T);
+    }
+
+    public T[] GetDataTable<T>()
+    {
+        if (!_isInit)
+        {
+            _isInit = true;
+            Init();
+        }
+        foreach (ScriptableObject s in _allDatas)
+        {
+            if (s.GetType().GetField("arrayData").FieldType.GetElementType() == typeof(T))
+            {
+                return (T[])s.GetType().GetField("arrayData").GetValue(s);
+            }
+        }
+        Debug.LogError("not find data type " + typeof(T));
+        return default(T[]);
+    }
+    public List<T> GetDataTable<T>(Predicate<T> match)
+    {
+        if (!_isInit)
+        {
+            _isInit = true;
+            Init();
+        }
+        foreach (ScriptableObject s in _allDatas)
+        {
+            if (s.GetType().GetField("arrayData").FieldType.GetElementType() == typeof(T))
+            {
+                return ((T[])s.GetType().GetField("arrayData").GetValue(s)).ToList().FindAll(match)
+            }
+        }
+        Debug.LogError("not find data type " + typeof(T));
+        return null;
+    }
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed
+    /// 싱글톤이 부서질 일이 없지만 Reset을 겪을 경우 새로 만들어 지기 때문에 필요하다.
+    /// </summary>
+    public void OnDestroy()
+    {
+        _isInit = false;
+    }
+}
