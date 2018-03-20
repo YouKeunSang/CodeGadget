8c7fdd48b69a958a8c5e2230221bd8260a27c436
You Ken Sang <youks95@bluegames.co.kr>
2018-03-20 16:16:40 +0900

    다국어 지원 기능 만듬 +메뉴아이템 동적으로 만들기 기술,유니크함 +스크린 리플레쉬 하는 방법 -data테이블과 밀결합 조심

---
+++ editor확장/MultiLanguageLabel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// UI에 대한 다국어 지원방법 구현
+/// </summary>
+[ExecuteInEditMode]
+public class MultiLanguageLabel : Text {
+    [HideInInspector]
+    public LANGUAGE_TYPE langType;
+    [HideInInspector]
+    public UI_TEXT_TYPE uiType;
+    [HideInInspector]
+    public string uiID;
+    [HideInInspector]
+    public EssentialRectTransfom defaultTransform;
+
+    public Dictionary<LANGUAGE_TYPE, EssentialRectTransfom> positionSnaps = new Dictionary<LANGUAGE_TYPE, EssentialRectTransfom>();
+
+    protected override void OnEnable()
+    {
+        //실제 플레이 상황에서는 옵션값에서 읽어온 값으로 표시하고
+        //에디터에서는 인스펙터에서 설정된 값으로 표시한다.
+        if(Application.isPlaying)
+        {
+            langType = GameManager.instance.language;
+        }
+        OnChangeLanguage();
+    }
+    [ExecuteInEditMode]
+    public void OnChangeLanguage(LANGUAGE_TYPE lang)
+    {
+        langType = lang;
+        OnChangeLanguage();
+    }
+    [ExecuteInEditMode]
+    public bool OnChangeLanguage()
+    {
+        if (!string.IsNullOrEmpty(uiID))
+        {
+            EssentialRectTransfom _trans;
+            if(positionSnaps.TryGetValue(langType,out _trans))
+            {
+                _trans.RestoreRect(this);
+            }
+            else if(null != defaultTransform)
+            {
+                defaultTransform.RestoreRect(this);
+            }
+
+            switch (uiType)
+            {
+                case UI_TEXT_TYPE.OBJECT:
+                    Language_objectInfo _objectInfo = DataManager.Instance.GetData<Language_objectInfo>(uiID);
+                    if (null == _objectInfo)
+                    {
+                        text = uiID;
+                        return false;
+                    }
+                    text = (string)_objectInfo.GetType().GetField(langType.ToString().ToLower()).GetValue(_objectInfo);
+                    break;
+                case UI_TEXT_TYPE.SYSTEM:
+                    Language_systemInfo _systemInfo = DataManager.Instance.GetData<Language_systemInfo>(uiID);
+                    if (null == _systemInfo)
+                    {
+                        text = uiID;
+                        return false;
+                    }
+                    text = (string)_systemInfo.GetType().GetField(langType.ToString().ToLower()).GetValue(_systemInfo);
+                    break;
+                case UI_TEXT_TYPE.UI:
+                    Language_uiInfo _info = DataManager.Instance.GetData<Language_uiInfo>(uiID);
+                    if(null == _info)
+                    {
+                        text = uiID;
+                        return false;
+                    }
+                    text = (string)_info.GetType().GetField(langType.ToString().ToLower()).GetValue(_info);
+                    break;
+                default:
+                    text = uiID;
+                    Debug.LogError("누군가 상의하지 않은 테이블을 만듬,잡아오셈");
+                    break;
+            }
+        }
+        return true;
+    }
+}
+
+/// <summary>
+/// rectTransform의 중요한 정보를 저장하고 언어가 바뀌었을때 원복해주는 클래스
+/// </summary>
+public class EssentialRectTransfom
+{
+    Vector2 _position;
+    Vector2 _size;
+    int _fontSize;
+    public EssentialRectTransfom(MultiLanguageLabel label)
+    {
+        _position = label.rectTransform.anchoredPosition;
+        _size = label.rectTransform.sizeDelta;
+        _fontSize = label.fontSize;
+    }
+    public void RestoreRect(MultiLanguageLabel label)
+    {
+        label.rectTransform.anchoredPosition = _position;
+        label.rectTransform.sizeDelta = _size;
+        label.fontSize = _fontSize;
+    }
+}
---
+++ editor확장/MultiLanguageLabelEditor.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Text;
+
+[CustomEditor(typeof(MultiLanguageLabel))]
+public class MultiLanguageLabelEditor : Editor {
+    MultiLanguageLabel _target;
+    bool _isDirty = false;
+    bool _isSnapFoldShow = false;
+
+    private void OnEnable()
+    {
+        _target = (MultiLanguageLabel)target;
+        if(_target.langType != GameManager.instance.language)
+        {
+            Debug.Log("not default language");
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("Multi Language", EditorStyles.boldLabel);
+        EditorGUI.BeginChangeCheck();
+        _target.uiID = EditorGUILayout.TextField("UI ID",_target.uiID);
+        _target.langType = (LANGUAGE_TYPE)EditorGUILayout.EnumPopup("language:", _target.langType);
+        _target.uiType = (UI_TEXT_TYPE)EditorGUILayout.EnumPopup("type:", _target.uiType);
+
+        //저장된 스냅샷을 표시함
+        _isSnapFoldShow = EditorGUILayout.Foldout(_isSnapFoldShow, "snapshot");
+        if(_isSnapFoldShow)
+        {
+            bool _isDelete = false;
+            LANGUAGE_TYPE _shouldDelete = LANGUAGE_TYPE.KOREAN;
+
+            if(null ==_target.defaultTransform)
+            {
+                _target.defaultTransform = new EssentialRectTransfom(_target);
+            }
+            foreach(KeyValuePair<LANGUAGE_TYPE, EssentialRectTransfom> t in _target.positionSnaps)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.SelectableLabel(t.Key.ToString());
+                if(GUILayout.Button("-"))
+                {
+                    _isDelete = true;
+                    _shouldDelete = t.Key;
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            if(_isDelete)
+            {
+                _target.positionSnaps.Remove(_shouldDelete);
+                _isDelete = false;
+            }
+            //스냅샷과 디폴트 위치를 설정하는 버튼들
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Snapshot"))
+            {
+                _target.positionSnaps[_target.langType] = new EssentialRectTransfom(_target);
+            }
+            if (GUILayout.Button("Make this Default"))
+            {
+                _target.defaultTransform = new EssentialRectTransfom(_target);
+            }
+            if (GUILayout.Button("Restore to Default"))
+            {
+                _target.defaultTransform.RestoreRect(_target);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            _target.OnChangeLanguage();
+        }
+
+        base.OnInspectorGUI();
+        
+    }
+
+}
+
+public class LanguageChanger : Editor
+{
+    [MenuItem("Language/reflesh lagnuages", false, 20)]
+    public static void LanguageChange()
+    {
+        string scriptFile = "Assets/Script/Editor/LanguageItemMenu.cs";
+        string[] menuItems = Enum.GetNames(typeof(LANGUAGE_TYPE));
+
+        // The class string
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("// This class is Auto-Generated");
+        sb.AppendLine("using UnityEngine;");
+        sb.AppendLine("using UnityEditor;");
+        sb.AppendLine("");
+        sb.AppendLine("  public static class GeneratedMenuItems {");
+        sb.AppendLine("");
+
+        // loops though the array and generates the menu items
+        for (int i = 0; i < menuItems.Length; i++)
+        {
+            sb.AppendLine("    [MenuItem(\"Language/" + menuItems[i] + "\")]");
+            sb.AppendLine("    private static void MenuItem" + i.ToString() + "() {");
+            sb.AppendLine("        LanguageChanger.RebuildLanguage( LANGUAGE_TYPE." + menuItems[i] + ");");
+            sb.AppendLine("    }");
+            sb.AppendLine("");
+        }
+
+        sb.AppendLine("");
+        sb.AppendLine("}");
+
+        // writes the class and imports it so it is visible in the Project window
+        System.IO.File.Delete(scriptFile);
+        System.IO.File.WriteAllText(scriptFile, sb.ToString(), System.Text.Encoding.UTF8);
+        AssetDatabase.ImportAsset(scriptFile);
+
+    }
+
+    public static void RebuildLanguage(LANGUAGE_TYPE type)
+    {
+        MultiLanguageLabel[] _objects = FindObjectsOfType<MultiLanguageLabel>();
+        foreach (MultiLanguageLabel o in _objects)
+        {
+            o.OnChangeLanguage(type);
+            EditorUtility.SetDirty(o);
+        }
+        SceneView.RepaintAll();
+    }
+}
+
